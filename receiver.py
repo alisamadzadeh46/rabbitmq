eb22e58ea20662ac2e28db6e0b8a5ae3caab7733
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-06-02 23:45:24 +0430

    update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,3 +1,5 @@
+import time
+
 import pika
 
 # if you use server , replace 'localhost' to ip server
@@ -9,23 +11,27 @@ channel = connection.channel()
 
 # create new queue
 # This method creates or checks a queue
-channel.queue_declare(queue='hi')
+channel.queue_declare(queue='one')
 
 
 # body: this parameter reading from within the queue
 def callback(ch, method, properties, body):
     print(f'Received {body}')
+    time.sleep(9)
+    print('Done')
+    channel.basic_ack(delivery_tag=method.delivery_tag)
 
 
 # Consume to the broker and binds messages
 # for the consumer_tag to the consumer callback
 channel.basic_consume(
-    queue='hi',
+    queue='one',
     on_message_callback=callback,
     auto_ack=True
 )
 
-print('waiting for message , to exit press ctrl+c')
+channel.basic_qos(prefetch_count=1)
+print('waiting for message , press ctrl+c to exit ')
 
 # start consuming
 channel.start_consuming()
