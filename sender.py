0b0b9d363bd676f1aa5e770eff8080fdaf1205d6
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-06-03 03:09:56 +0430

    create sender.py

--- sender.py
+++ sender.py
@@ -7,10 +7,8 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost')
 # create new channel
 channel = connection.channel()
 
-# create new queue
-# This method creates or checks a queue
-# when True durable, when restart server or crash information  write in hard , Survive reboots of the broker
-channel.queue_declare(queue='one', durable=True)
+# create new exchange
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
 # create message replace command line or terminal
 message = 'This is testing message'
@@ -18,10 +16,9 @@ message = 'This is testing message'
 # send simple message
 # if empty exchange,means direct exchange
 channel.basic_publish(
-    exchange='',
-    routing_key='one',
+    exchange='logs',
+    routing_key='',
     body=message,
-    properties=pika.BasicProperties(delivery_mode=2,)
 )
 
 print('Message send')
