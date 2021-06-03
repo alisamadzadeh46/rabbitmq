b5c36d6a0972ff612b52d57db36f68cf23b151bb
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-06-03 18:24:28 +0430

    create sender.py

--- sender.py
+++ sender.py
@@ -8,22 +8,23 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost')
 channel = connection.channel()
 
 # create new exchange
-channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
 
 # create dictionary message
 message = {
-    'info': 'this is information message',
-    'error': 'this is error message',
-    'warning': 'this is warning message',
+    'info.debug,NotImportant': 'this is an not important message',
+    'error.warning.important': 'this is an important message',
 }
 
 # access to message ,k:key , v : value
 for k, v in message.items():
     channel.basic_publish(
-        exchange='direct_logs',
+        exchange='topic_logs',
         routing_key=k,
         body=v,
     )
+
+print('Sent')
 
 # this method close connection
 connection.close()
